281a294c26c22ee80503875f0bb22b56d64b2159
Chinmay M <95156077+chinmaym505@users.noreply.github.com>
2025-05-17 21:42:51 -0400

    Delete mac directory
    Removed support for mac

--- mac/setup.py
+++
@@ -1,8 +0,0 @@
-from PyInstaller.__main__ import run
-
-run([
-    'gitFlow.py',
-    '--onefile',  # Package as a single executable
-    '--console',  # Ensure it runs in Terminal
-    '--name=gitFlow'  # Name the final binary
-])
